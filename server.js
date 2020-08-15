38739bb3b4c9f7454b83e205f0ed64975900cd0f
SKG-0 <sarthakguptamailbox@gmail.com>
2020-08-15 21:37:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose');
+const express=require('express');
+const app=express();
+const expressLayouts=require('express-ejs-layouts');
+const bodyParser=require('body-parser');
+const methodOverride=require('method-override');
+const indexRouter=require('./routes/index');
+const authorRouter=require('./routes/authors');
+const bookRouter=require('./routes/books');
+const path=require('path');
+app.set('view-engine','ejs');
+app.set('views',__dirname+'/views');
+app.set('layout','layouts/layout.ejs');
+app.use(expressLayouts);
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+mongoose.connect('mongodb://localhost/book-keeper',{useNewUrlParser:true,useUnifiedTopology:true});
+const db=mongoose.connection;
+db.on('error',error=> console.error(error));
+db.once('open',()=> console.log("Connected to mongoose"));
+app.use('/',indexRouter);
+app.use('/authors',authorRouter);
+app.use('/books',bookRouter)
+app.listen(process.env.PORT||5000, console.log("Server started"));
